ce9e2f82fe20e0f2d3e55ebe803a08f0580ddb4b
bloriot97 <bloriot97@gmail.com>
2019-12-04 11:09:09 -0500

    add icon

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-export const uri = 'http://51.38.235.157:3003/api/v1';
-//export const uri = 'http://localhost:3003/api/v1';
+//export const uri = 'http://51.38.235.157:3003/api/v1';
+export const uri = 'http://localhost:3003/api/v1';
 export const version = 'v1'
 
 export const getUri = (path) => `${uri}${path}`;
